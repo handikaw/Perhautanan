c314aeba77e1c2120da4d7561188002d464fc082
handikaw <handikawijaksono88@gmail.com>
2026-07-04 00:21:34 +0700

    fix: enforce mysql pdo timeout options

--- config/database.php
+++ config/database.php
@@ -59,9 +59,12 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            // 👇 BAGIAN OPTIONS SUDAH DIPERBARUI UNTUK MENCEGAH TIMEOUT JALUR PUBLIK RAILWAY
+            'options' => array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                PDO::ATTR_TIMEOUT => 15, 
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'",
+            ]),
         ],
 
         'mariadb' => [
@@ -87,11 +90,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL', env('DATABASE_URL')),
-            'host' => env('DB_HOST', env('PGHOST', '127.0.0.1')),
-            'port' => env('DB_PORT', env('PGPORT', '5432')),
-            'database' => env('DB_DATABASE', env('PGDATABASE', 'forge')),
-            'username' => env('DB_USERNAME', env('PGUSER', 'forge')),
-            'password' => env('DB_PASSWORD', env('PGPASSWORD', '')),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
