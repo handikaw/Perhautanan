30b8c8e13cc642ed41fca0ec3c0c9cf48573d511
handikaw <handikawijaksono88@gmail.com>
2026-07-03 18:20:53 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,8 +10,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
