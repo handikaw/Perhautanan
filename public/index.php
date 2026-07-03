7ec29570dccd09aad00e7e2b01de9653a898ff96
handikaw <handikawijaksono88@gmail.com>
2026-07-03 19:34:56 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,18 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+if (isset($_GET['diagnose'])) {
+    header('Content-Type: text/plain');
+    echo "--- ENVIRONMENT DIAGNOSTICS ---\n";
+    foreach (getenv() as $key => $value) {
+        if (stripos($key, 'pass') !== false || stripos($key, 'key') !== false || stripos($key, 'secret') !== false) {
+            $value = substr($value, 0, 3) . '***';
+        }
+        echo "$key=$value\n";
+    }
+    exit;
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
